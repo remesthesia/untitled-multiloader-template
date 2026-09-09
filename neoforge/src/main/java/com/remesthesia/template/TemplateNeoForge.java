8aa6b06f82207fb1df5d5cc3f87266aa39558aea
Halloween <281669769+remesthesia@users.noreply.github.com>
2026-09-09 14:44:02 -0400

    Refactor entrypoints and mixins.json

--- neoforge/src/main/java/com/remesthesia/template/TemplateNeoForge.java
+++ neoforge/src/main/java/com/remesthesia/template/TemplateNeoForge.java
@@ -4,9 +4,9 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
-public class ExampleMod {
+public class TemplateNeoForge {
 
-    public ExampleMod(IEventBus eventBus) {
+    public TemplateNeoForge(IEventBus eventBus) {
 
         // This method is invoked by the NeoForge mod loader when it is ready
         // to load your mod. You can access NeoForge and Common code in this
@@ -14,7 +14,7 @@ public class ExampleMod {
 
         // Use NeoForge to bootstrap the Common mod.
         Constants.LOG.info("Hello NeoForge world!");
-        CommonClass.init();
+        Template.init();
 
     }
 }
