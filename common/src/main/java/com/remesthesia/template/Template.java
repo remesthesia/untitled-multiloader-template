3dae455d7416c1c14a87eaece3827684e2a39e71
Halloween <281669769+remesthesia@users.noreply.github.com>
2026-09-10 09:51:50 -0400

    Remove unneeded logging and comments

--- common/src/main/java/com/remesthesia/template/Template.java
+++ common/src/main/java/com/remesthesia/template/Template.java
@@ -1,30 +1,11 @@
 package com.remesthesia.template;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
-// import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
-// common compatible binaries. This means common code can not directly use loader specific concepts such as NeoForge events
-// however it will be compatible with all supported mod loaders.
 public class Template {
-
     public static final String MOD_ID = "template";
-    public static final Logger LOG = LoggerFactory.getLogger("Template");
+    public static final Logger LOGGER = LoggerFactory.getLogger("Template");
 
-    // The loader specific projects are able to import and use any code from the common project. This allows you to
-    // write the majority of your code here and load it from your loader specific projects. This example has some
-    // code that gets invoked by the entry point of the loader specific projects.
-    public static void init() {
-
-        LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
-
-        // It is common for all supported loaders to provide a similar feature that can not be used directly in the
-        // common code. A popular way to get around this is using Java's built-in service loader feature to create
-        // your own abstraction layer. You can learn more about this in our provided services class. In this example
-        // we have an interface in the common code and use a loader specific implementation to delegate our call to
-        // the platform specific approach.
-    }
+    public static void init() {}
 }
--- neoforge/src/main/java/com/remesthesia/template/TemplateNeoForge.java
+++ neoforge/src/main/java/com/remesthesia/template/TemplateNeoForge.java
@@ -7,14 +7,6 @@ import net.neoforged.fml.common.Mod;
 public class TemplateNeoForge {
 
     public TemplateNeoForge(IEventBus eventBus) {
-
-        // This method is invoked by the NeoForge mod loader when it is ready
-        // to load your mod. You can access NeoForge and Common code in this
-        // project.
-
-        // Use NeoForge to bootstrap the Common mod.
-        Template.LOG.info("Hello NeoForge world!");
         Template.init();
-
     }
 }
