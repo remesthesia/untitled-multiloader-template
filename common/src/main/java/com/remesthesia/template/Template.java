47b910f3de915c1ec6465aebf98b4dececcac68c
Halloween <281669769+remesthesia@users.noreply.github.com>
2026-09-10 09:47:27 -0400

    Clean up missing references

--- common/src/main/java/com/remesthesia/template/Template.java
+++ common/src/main/java/com/remesthesia/template/Template.java
@@ -1,6 +1,5 @@
 package com.remesthesia.template;
 
-import com.remesthesia.template.platform.Services;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
@@ -15,7 +14,6 @@ public class Template {
     // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
 
-        Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
         Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
